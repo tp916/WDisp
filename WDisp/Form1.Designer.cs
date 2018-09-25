d94a2a53581ee44f5e84e95b4096dd02c45bb830
916 <916@laptop>
2018-09-25 21:30:07 +0200

    start

---
+++ WDisp/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WDisp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelGraph = new System.Windows.Forms.Panel();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelGraph
+            // 
+            this.panelGraph.AutoScroll = true;
+            this.panelGraph.BackColor = System.Drawing.SystemColors.Window;
+            this.panelGraph.Location = new System.Drawing.Point(0, 0);
+            this.panelGraph.Name = "panelGraph";
+            this.panelGraph.Size = new System.Drawing.Size(778, 483);
+            this.panelGraph.TabIndex = 0;
+            this.panelGraph.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGraph_Paint);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(15, 437);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 1;
+            this.buttonRefresh.Text = "refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonRefresh);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(93, 483);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(778, 483);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelGraph);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelGraph;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WDisp/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WDisp
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics g = null;
+        Pen myPen;
+        static int centerX;
+        static int centerY;
+
+        public Form1()
+        {
+            InitializeComponent();
+            myPen = new Pen(Color.DarkGreen,1);
+            g = panelGraph.CreateGraphics();
+            /*
+            panelGraph.MaximumSize = new Size(500, 500);
+            panelGraph.Size = new Size(500, 500);
+            panelGraph.Size.Width = 200;
+            */
+           // panelGraph.Size = new Size(Form1.Size.Width);
+
+            centerX = panelGraph.Width / 2;
+            centerY = panelGraph.Height / 2;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void testGraphics()
+        {
+            //g.DrawEllipse(myPen, 100, 100, 50, 50);
+            int CenterX = panelGraph.Width / 2;
+            int CenterY = panelGraph.Height / 2;
+            g.DrawLine(myPen, CenterX, CenterY, 200, 200);
+        }
+
+
+        private void drawLineByAngle(int startX, int startY, int len, double angle, int startOfset)
+        {
+            double radian = (angle * Math.PI) / 180;
+            float stopX = (float) (len * Math.Cos(radian));
+            float stopY = (float) (len * Math.Sin(radian));
+
+            startX = (int)(startOfset * Math.Cos(radian));
+            startY = (int)(startOfset * Math.Sin(radian));
+
+            startX += centerX;
+            startY += centerY;
+            stopX += centerX;
+            stopY += centerY;
+    
+            g.DrawLine(myPen, startX, startY, stopX, stopY);
+    
+        }
+
+
+        private void drawGrid(int numbOfLines, int numbOfLeds) 
+        {
+             
+            double angle = 0;
+            for (int i = 0; i < numbOfLines; i++)
+            {
+                drawLineByAngle(0, 0, centerY,angle, centerY/4);
+                angle += ((double)360 / numbOfLines); 
+            }
+    
+        }
+
+
+         
+        private void panelGraph_Paint(object sender, PaintEventArgs e)
+        {
+            //testGraphics();
+            
+            g.Dispose();
+            g = panelGraph.CreateGraphics();
+            g.Clear(Color.White);
+
+            centerX = panelGraph.Width / 2;
+            centerY = panelGraph.Height / 2;
+            drawGrid(100, 10);
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            panelGraph.Refresh();
+           
+        }
+    }
+
+
+   
+}
+
+
